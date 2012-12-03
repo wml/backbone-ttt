7526d343368f0611fc50f3766d35c56a79ef54e2
William Leszczuk <william.leszczuk@gmail.com>
2012-12-03 16:44:36 -0500

    initial rails boilerplate

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BackboneTtt::Application.config.secret_token = 'abcd8dcc5aa5a975a3a89531da1ece4b4bb18266624000527d765ecda9cdf085e2380da413cc4ccaaf03c57da6b1712393ca4c27cfc96377316730dc61c13694'
