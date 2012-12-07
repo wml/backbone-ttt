d9c25841fbacc2ed72350d79e6cc7a41336eb3bd
William Leszczuk <william.leszczuk@gmail.com>
2012-12-06 23:01:13 -0500

    commit outstanding changes to V1 minimax algorithm - bug in blocking demonstrated by failing test

--- app/ai.rb
+++ app/ai.rb
@@ -14,16 +14,97 @@ module AI
           end
         end
       end
+
     end
   end
 
   class Minimax
     def self.move state
       state = JSON.parse(state)
+      return JSON.dump(self.algo(true, state, true))
+    end
 
-      # TODO
+    def self.algo myturn, board, root
+      score = self.score(board)
+      children = self.children(myturn, board)
 
-      return JSON.dump(state)
+      if 0 == children.count or 0 != score
+        return score
+      end
+
+      bestscore = nil
+      bestmove = nil
+
+      children.each do |child|
+        score = self.algo((not myturn), child, false)
+
+        if nil == bestscore or \
+          ((myturn and score > bestscore) or (not myturn and score < bestscore))
+          bestscore = score
+          bestmove = child
+        end
+      end
+
+      if root
+        return bestmove
+      else
+        return bestscore
+      end
+    end
+
+    def self.children myturn, board
+      results = []
+      for row in 0..2
+        for col in 0..2
+          if board[row][col] == Game.States[:Open]
+            results += [self.get_child(board, myturn, row, col)]
+          end
+        end
+      end
+      return results
+    end
+
+    def self.get_child board, myturn, row, col
+      result = [[],[],[]]
+
+      for r in 0..2
+        for c in 0..2
+          result[r][c] = board[r][c]
+        end
+      end
+
+      result[row][col] = (myturn ? Game.States[:Opponent] : Game.States[:Human])
+      return result
+    end
+
+    def self.score board
+      scores = {
+        Game.States[:Opponent] => 1,
+        Game.States[:Human] => -1,
+        Game.States[:Open] => 0,
+      }
+
+      for row in 0..2
+        if board[row][0] == board[row][1] and board[row][1] == board[row][2]
+          return scores[board[row][0]]
+        end
+      end
+
+      for col in 0..2
+        if board[0][col] == board[1][col] and board[1][col] == board[2][col]
+          return scores[board[0][col]]
+        end
+      end
+
+      if board[0][0] == board[1][1] and board[1][1] == board[2][2]
+          return scores[board[0][0]]
+      end
+
+      if board[2][0] == board[1][1] and board[1][1] == board[0][2]
+          return scores[board[2][0]]
+      end
+        
+      return 0
     end
   end
 end
---
+++ test/unit/ai_test.rb
@@ -0,0 +1,22 @@
+require "test/unit"
+
+class MinimaxTest < Test::Unit::TestCase
+  def setup
+    @underTest = AI::Minimax
+  end
+
+  def test_only_move_is_taken
+    onlyOneMove = '[[1,2,1],[1,2,2],[2,1,0]]'
+    assert_equal('[[1,2,1],[1,2,2],[2,1,2]]', @underTest.move(onlyOneMove))
+  end
+
+  def test_winning_move_is_taken
+    oneWinningMove = '[[1,1,2],[2,1,2],[1,0,0]]'
+    assert_equal('[[1,1,2],[2,1,2],[1,0,2]]', @underTest.move(oneWinningMove))
+  end
+
+  def test_forced_block_taken
+    oneWinningMove = '[[1,2,0],[1,0,0],[0,0,0]]'
+    assert_equal('[[1,2,0],[1,0,0],[2,0,0]]', @underTest.move(oneWinningMove))
+  end
+end
