c5399536336ede0eaa5c8805feb274c0490e1ad1
William Leszczuk <william.leszczuk@gmail.com>
2012-12-30 14:49:25 -0500

    implemented first cut of heuristic algorithm and enabled AI in UI.

--- app/ai.rb
+++ app/ai.rb
@@ -7,27 +7,28 @@ require 'game'
 #       - make sloppy minimax take a random position instead of 1st open
 
 module AI
-  class FirstAvailable 
-    def self.move state
-      state = JSON.parse(state)
-
-      0.upto(state.count - 1) do |row|
-        0.upto(state[row].count - 1) do |col|
-          if Game.States[:Open] == state[row][col]
-            state[row][col] = Game.States[:Opponent]
-            return JSON.dump(state)
-          end
+  module_function
+  def _take_first_available state
+    0.upto(state.count - 1) do |row|
+      0.upto(state[row].count - 1) do |col|
+        if Game.States[:Open] == state[row][col]
+          state[row][col] = Game.States[:Opponent]
+          return state
         end
       end
+    end
+  end
 
+  class FirstAvailable 
+    def self.move state
+      return JSON.dump(AI._take_first_available(JSON.parse(state)))
     end
   end
 
   class Minimax
     def self.move state
       state = JSON.parse(state)
-      solutions = { }
-      return JSON.dump(self.algo(true, state, true, solutions, 1, -100, 100))
+      return JSON.dump(self.algo(true, state, true, { }, 1, -100, 100))
     end
 
     def self.algo myturn, board, root, solutions, depth, alpha, beta
@@ -116,20 +117,128 @@ module AI
     end
   end
 
-  class Sloppy
-    def self.move state
-      if 0 == rand(10)
-        return FirstAvailable.move state
-      end
-      return Minimax.move state
-    end
-  end
-
   class Heuristic
     def self.move state
+      rules = [
+        :winning_move_or_block,
+        :fork_or_block_fork,
+        :center,
+        :opposite_corner_or_any_corner,
+        :first_available
+      ]
+      
+      state = JSON.parse(state)
+
+      rules.each do |rule|
+        if Heuristic.send rule, state
+          return JSON.dump(state)
+        end
+      end
+    end
+   
+    def self.winning_move_or_block state
+      won = self._eval_wins(
+        state, lambda {|humanslots, opponentslots| opponentslots == 2 }
+      )
+
+      if won 
+        return true
+      end
+
+      return self._eval_wins(
+        state, lambda {|humanslots, opponentslots| humanslots == 2 }
+      )
+    end
+
+    def self._eval_wins state, condition_for_move
+      initstate = lambda do
+        return {
+          :Open => -1,
+          :Human => 0,
+          :Opponent => 0
+        }
+      end
+
+      updatestate = lambda do |slot, linestate, idx|
+        case slot
+          when Game.States[:Open]: linestate[:Open] = idx
+          when Game.States[:Human]: linestate[:Human] += 1
+          else linestate[:Opponent] += 1
+        end
+      end
+
+      0.upto(state.count - 1) do |row|
+        linestate = initstate.call()
+
+        0.upto(state[row].count - 1) do |col|
+          updatestate.call(state[row][col], linestate, col)
+        end
+
+        if linestate[:Open] != -1 and \
+          condition_for_move.call(linestate[:Human], linestate[:Opponent])
+          state[row][linestate[:Open]] = Game.States[:Opponent]
+          return true
+        end
+      end
+
+      0.upto(state.count - 1) do |col|
+        linestate = initstate.call()
+
+        0.upto(state[col].count - 1) do |row|
+          updatestate.call(state[row][col], linestate, row)
+        end
+
+        if linestate[:Open] != -1 and \
+          condition_for_move.call(linestate[:Human], linestate[:Opponent])
+          state[linestate[:Open]][col] = Game.States[:Opponent]
+          return true
+        end
+      end
+  
+      linestate = initstate.call()
+      0.upto(2) do |rowcol|
+        updatestate.call(state[rowcol][rowcol], linestate, rowcol)
+      end
+      if linestate[:Open] != -1 and \
+        condition_for_move.call(linestate[:Human], linestate[:Opponent])
+        state[linestate[:Open]][linestate[:Open]] = Game.States[:Opponent]
+        return true
+      end
+
+      linestate = initstate.call()
+      0.upto(2) do |rowcol|
+        updatestate.call(state[2 - rowcol][rowcol], linestate, rowcol)
+      end
+      if linestate[:Open] != -1 and \
+        condition_for_move.call(linestate[:Human], linestate[:Opponent])
+        state[2 - linestate[:Open]][linestate[:Open]] = Game.States[:Opponent]
+        return true
+      end
+
+      return false
+    end
+
+    def self.fork_or_block_fork state
+      # TODO
+      return false
+    end
+
+    def self.center state
+      # TODO
+      return false
+    end
+
+    def self.opposite_corner_or_any_corner state
+      # TODO
+      return false
+    end
+
+    def self.first_available state
+      AI._take_first_available state
+      return true
+    end
+
       # TODO --------------------
-      # 1. check for win
-      # 2. block opponent win
       # 3. check for fork
       # 4. block opponent's fork
       #    1. by setting up 2 in a row to force a block, so long as the block doesn't allow for a fork (diagonal with you in center, dont play corner)
@@ -139,6 +248,14 @@ module AI
       # 7. play any corner if available
       # 8. play a side
       # END algorithm
+  end
+
+  class Sloppy
+    def self.move state
+      if 0 == rand(10)
+        return FirstAvailable.move state
+      end
+      return Minimax.move state
     end
   end
 end
--- test/unit/ai_test.rb
+++ test/unit/ai_test.rb
@@ -25,3 +25,59 @@ class MinimaxTest < Test::Unit::TestCase
     assert_equal('[[1,2,0],[1,0,0],[2,0,0]]', @underTest.move(forcedBlock))
   end
 end
+
+class HeuristicTest <  Test::Unit::TestCase
+  def setup
+    @underTest = AI::Heuristic
+  end
+
+  def test_winning_row_move_is_taken
+    oneWinningMove = '[[1,2,1],[2,2,0],[1,1,0]]'
+    assert_equal('[[1,2,1],[2,2,2],[1,1,0]]', @underTest.move(oneWinningMove))
+  end
+
+  def test_winning_row_move_is_taken_over_block
+    winOrBlock = '[[1,1,0],[2,2,0],[1,0,0]]'
+    assert_equal('[[1,1,0],[2,2,2],[1,0,0]]', @underTest.move(winOrBlock))
+  end
+
+  def test_row_block_is_taken_when_no_win_present
+    oneBlock = '[[1,1,0],[2,0,0],[0,0,0]]'
+    assert_equal('[[1,1,2],[2,0,0],[0,0,0]]', @underTest.move(oneBlock))
+  end
+
+  def test_winning_col_move_is_taken
+    oneWinningMove = '[[1,2,1],[2,2,1],[1,0,0]]'
+    assert_equal('[[1,2,1],[2,2,1],[1,2,0]]', @underTest.move(oneWinningMove))
+  end
+
+  def test_winning_col_move_is_taken_over_block
+    winOrBlock = '[[1,2,1],[1,2,0],[0,0,0]]'
+    assert_equal('[[1,2,1],[1,2,0],[0,2,0]]', @underTest.move(winOrBlock))
+  end
+
+  def test_col_block_is_taken_when_no_win_present
+    oneBlock = '[[1,2,0],[1,0,0],[0,0,0]]'
+    assert_equal('[[1,2,0],[1,0,0],[2,0,0]]', @underTest.move(oneBlock))
+  end
+
+  def test_ulbr_diagonal_win_taken
+    diagonalWin = '[[2,1,1],[1,2,0],[0,0,0]]'
+    assert_equal('[[2,1,1],[1,2,0],[0,0,2]]', @underTest.move(diagonalWin))
+  end
+
+  def test_ulbr_diagonal_block_taken
+    diagonalBlock = '[[1,2,0],[2,1,0],[0,1,0]]'
+    assert_equal('[[1,2,0],[2,1,0],[0,1,2]]', @underTest.move(diagonalBlock))
+  end
+
+  def test_blur_diagonal_win_taken
+    diagonalWin = '[[1,0,0],[1,2,1],[2,0,0]]'
+    assert_equal('[[1,0,2],[1,2,1],[2,0,0]]', @underTest.move(diagonalWin))
+  end
+
+  def test_blur_diagonal_block_taken
+    diagonalBlock = '[[0,2,1],[0,0,0],[1,0,0]]'
+    assert_equal('[[0,2,1],[0,2,0],[1,0,0]]', @underTest.move(diagonalBlock))
+  end
+end
